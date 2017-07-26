5d35f83318ad8e2d43139a43742e36834ee862a0
houwenhao4147 <houwenhao4147@163.com>
2017-07-26 09:42:45 +0800

    COMMIT bug

--- app/src/main/java/com/hou/gitdemo/MainActivity.java
+++ app/src/main/java/com/hou/gitdemo/MainActivity.java
@@ -11,5 +11,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Log.e("TAG", "MainActivity");
         Log.e("TAG", "DEV");
+        Log.e("TAG", "DEV2");
     }
 }
+
